210d3624e9bfd9cb1a7a6054a3d200a1a176af3f
thepjos <josh.thephasdin@gmail.com>
2023-03-02 20:14:47 -0600

    fixed picture

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
         const img = btn.querySelector('img')
-        const src = img.getAttribute('scr')
+        const src = img.getAttribute('src')
         const alt = img.getAttribute('alt')
         selected_insect = { src, alt }
         screens[1].classList.add('up')
